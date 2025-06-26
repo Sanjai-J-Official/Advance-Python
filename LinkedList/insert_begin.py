566046921a599c1321c84837bc9524801b1a83c2
Sanjai <167332993+Sanjai-J-Official@users.noreply.github.com>
2025-06-26 11:17:41 +0530

    Create insert_begin.py

---
+++ LinkedList/insert_begin.py
@@ -0,0 +1,38 @@
+class Node:
+   def __init__(self,val):
+     self.val=val
+     self.next=None 
+    
+lis=[10,20,30,40]
+
+
+ 
+def insert_lis(lis):
+  head=Node(lis[0])
+  current=head
+   
+  
+  for i in lis[1:]:
+    current.next=Node(i)
+    current=current.next
+   
+  return head 
+  
+  
+head=insert_lis(lis)
+def print_all(head):
+  current=head
+  while current:
+    print(current.val, end="->")
+    current=current.next
+  print(None)
+  
+value=100
+
+def insert_begin(head,value):
+   current=Node(100)
+   current.next=head 
+   return current
+  
+head=insert_begin(head,value)  
+print_all(head)
